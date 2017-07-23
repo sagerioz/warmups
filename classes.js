41c40b6024e2a90bf7ea2e57066a0c956dd77a4e
sagerioz <ericaepperson27@yahoo.com>
2017-07-23 01:28:09 -0600

    more on classes

--- classes.js
+++ classes.js
@@ -15,3 +15,21 @@ Person.prototype.hometown = this.hometown;
 let father = new Person("Sterlie", "Epperson", 50)
 father.hometown = "Downieville"
 console.log(father.bio());
+
+function Animal(name, sound, food){
+  this.name = name;
+  this.sound = sound;
+  this.food = food;
+}
+
+let cow = new Animal("Floozie", "Mooo!", "hay")
+console.log(cow)
+
+Animal.prototype.breed = this.breed;
+
+let dog = new Animal("Elmer", "ruff!", "bacon, please")
+dog.breed = "collie";
+console.log(dog)
+
+Animal.prototype.message = function(){return "I am " + this.name + " and I love to eat " + this.food + "!"};
+console.log(dog.message())
